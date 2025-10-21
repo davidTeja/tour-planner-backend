25a2a5e387848ef5e73979f2e171475db69c30af
David Teja <pteja721hotmail@gmail.com>
2025-10-21 22:20:01 +0530

    Add .env.production.local for production environment configuration and update config.js comments

--- Server/configuration/config.js
+++ Server/configuration/config.js
@@ -1,8 +1,14 @@
 const dotenv = require("dotenv");
 
+// I'm accessing env before even loading the file, hence NODE_ENV is undefined
 dotenv.config({
   path: `./.env.${process.env.NODE_ENV || "development"}.local`,
 });
 
+// console.log("Current Environment:", process.env.NODE_ENV);
+
 const { PORT, NODE_ENV, MONGO_URI } = process.env;
 module.exports = { PORT, NODE_ENV, MONGO_URI };
+
+// NODE_ENV are set per session in powershell unlike in cmd.
+// command to set the env explicitly in powershell:  $env:NODE_ENV="development"
