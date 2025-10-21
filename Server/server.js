5df9f49ce63517735a8b7f231ac7d6dc4138f104
David Teja <pteja721hotmail@gmail.com>
2025-10-22 03:20:47 +0530

    Integrate static frontend into backend. App is ready!

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+
+const path = require("path");
+
 const { connectDB } = require("./configuration/db.js");
 const { PORT, NODE_ENV } = require("./configuration/config.js");
 const tourRoutes = require("./routes/tour.routes.js");
@@ -10,19 +13,36 @@ const {
 } = require("./middlewares/errorHandler.js");
 
 const app = express();
+// Connect to Database
 connectDB();
 
+// Middlewares
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Tour Planner!</h1>");
-});
+if (NODE_ENV === "production") {
+  // Serve static files from "public" folder in production
+  const publicPath = path.join(__dirname, "public");
+  app.use(express.static(publicPath));
+} else {
+  // For local development testing
+  app.get("/", (req, res) => {
+    res.send(
+      `<h1>Welcome to Tour Planner!</h1>
+     <a href="http://localhost:3000/Client/index.html">Let's Tour!</a>`
+    );
+  });
+}
+
+// API routes
 app.use("/api/v1/", tourRoutes);
+
+// Error handlers
 app.use(wrongRouteHandler);
 app.use(globalErrorHandler);
 
+// Start server
 app.listen(PORT, () => {
   if (NODE_ENV === "production") {
     console.log(`Server running on PORT:${PORT}`);
