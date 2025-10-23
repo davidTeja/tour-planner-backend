4708db0d5ec944a4de108c40b9c7b0bff5d66c58
pteja721hotmail@gmail.com <pteja721hotmail@gmail.com>
2025-10-24 01:40:15 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI5NjkzMA/spectral.js?branch_slug=main&token=13e70fd1-6aa5-4848-bf41-87472e72d85f";
+export default { extends: ruleset };
